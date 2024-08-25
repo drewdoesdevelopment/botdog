c55f65f3e9cb8c65cfee51e5aa1e77182e33f234
Drew_Pfeiler <drewcpfeiler@gmail.com>
2024-08-25 18:52:37 -0500

    Adds connection code and dotenv package

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
 
-func main () {
-    fmt.Println("test123");
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	loginBot()
+}
+
+func loginBot() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading env file in main.go!")
+	}
+
+	botApiKey := os.Getenv("BOT_API_SECRET")
+    fmt.Println(botApiKey)
+
+	discord, discLibErr := discordgo.New("Bot " + botApiKey)
+
+    if discLibErr != nil {
+        log.Fatal(discLibErr)
+    }
+
+    connectionErr := discord.Open()
+    if connectionErr != nil {
+        log.Fatal(connectionErr)
+    }
+    defer discord.Close()
+
+    fmt.Println("Botdog running...")
+    ch := make(chan os.Signal, 1)
+    signal.Notify(ch, os.Interrupt)
+    <- ch
 }
