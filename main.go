74861baf350c2fdd41e72dc1283cdff2ebbbb40f
Drew_Pfeiler <drewcpfeiler@gmail.com>
2024-08-27 18:17:15 -0500

    Removes debug

--- main.go
+++ main.go
@@ -16,12 +16,12 @@ func main() {
 
 func loginBot() {
 	err := godotenv.Load()
+
 	if err != nil {
 		log.Fatal("Error loading env file in main.go!")
 	}
 
 	botApiKey := os.Getenv("BOT_API_SECRET")
-	fmt.Println(botApiKey)
 
 	discord, discLibErr := discordgo.New("Bot " + botApiKey)
 
@@ -33,10 +33,11 @@ func loginBot() {
 	if connectionErr != nil {
 		log.Fatal(connectionErr)
 	}
+
 	defer discord.Close()
 
 	fmt.Println("Botdog running...")
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, os.Interrupt)
-	<- ch
+	<-ch
 }
